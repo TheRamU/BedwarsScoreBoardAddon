9b8102e58e57a736f52545fcf50685181de4ae9b
TheRam_ <43772379+TheRamU@users.noreply.github.com>
2021-07-14 21:58:09 +0800

    2.13.0

--- src/main/java/me/ram/bedwarsscoreboardaddon/menu/MenuType.java
+++
@@ -1,5 +0,0 @@
-package me.ram.bedwarsscoreboardaddon.menu;
-
-public enum MenuType {
-	EDIT, EDIT_TEAM, EDIT_TEAM_BED, EDIT_TEAM_SPAWN, EDIT_SPAWNER, EDIT_TEAM_SPAWNER, EDIT_GAME_SPAWNER, TEAM_SHOP, TEAM_SHOP_TRAP
-}
